8fbd4a1165af6f9cc77f302cc5ba46a2f9cdde8a
Gabriela DBS <gabrieladisalvatore@gmail.com>
2020-02-07 11:17:15 +0000

    finished

--- js/script.js
+++ js/script.js
@@ -1,3 +1,33 @@
 $(document).ready(function(){
- // put your code here
+
+   /* $(".theButton").click(function(){
+        $('#panel .container').siblings().hide();
+    }); */
+
+    /*$('.theButton').click(function(){
+        $(this).hide();
+    });*/
+
+   /* $('.theButton').click(function(){
+        $('#panel .container').siblings().fadeTo('slow', 0.1);
+    });
+
+    $('.superButton').on('click', function(){
+        $('#panel .container').siblings().fadeTo(1000, 1);
+    })*/
+
+    /*$('.theButton').on('mouseenter', function(){
+        $(this).css('background-color', '#000');
+    });*/
+
+    $('.theButton').on('mouseenter', function(){
+        $(this).addClass('makeBlack');
+    });
+
+    $('.theButton').on('mouseout', function(){
+        $(this).removeClass('makeBlack');
+    });
+
+
+
 });
